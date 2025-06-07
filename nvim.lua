afecb4a57e2fca93a8718cbc1ed2af4dcee143b0
alex-mackechnie <alex.mackechnie@skyscanner.net>
2025-06-07 12:02:37 +0100

    Moving to new config;

---
+++ nvim.lua
@@ -0,0 +1,482 @@
+-- Set leader keys
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Editor settings
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
+vim.opt.cursorline = true
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.opt.signcolumn = 'yes'
+
+-- Setup lazy.nvim
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  {
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    },
+    config = function()
+      require('telescope').setup {
+        defaults = {
+          vimgrep_arguments = {
+            'rg', '--color=never', '--no-heading', '--with-filename',
+            '--line-number', '--column', '--smart-case', '--fixed-strings'
+          },
+          file_ignore_patterns = { '*test*', '.git/', 'node_modules/' }
+        },
+        pickers = {
+          find_files = { hidden = true }
+        },
+        extensions = {
+          ['ui-select'] = require('telescope.themes').get_dropdown(),
+        },
+      }
+      pcall(require('telescope').load_extension, 'fzf')
+      pcall(require('telescope').load_extension, 'ui-select')
+
+      local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>fg', function()
+        builtin.live_grep {
+          vimgrep_arguments = vim.list_extend({
+            'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--hidden', '--fixed-strings'
+          }, {
+            '--glob=!**/node_modules/', '--glob=!**/.git/', '--glob=!**/package-lock.json', '--glob=!**/*test*'
+          }),
+        }
+      end, { desc = 'Live grep (no test/git/node_modules/lock)' })
+    end,
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'go', 'java', 'python', 'javascript'},
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.install').prefer_git = true
+      require('nvim-treesitter.configs').setup(opts)
+    end,
+  },
+
+  {
+    "pmizio/typescript-tools.nvim",
+    ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" }, -- CHANGED
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+	opts = {
+	  settings = {
+		typescript = {
+		  tsserver = {
+			maxTsServerMemory = 8192, -- CHANGED (try 2048 or 4096)
+		  },
+		},
+	  },
+	}
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      { 'j-hui/fidget.nvim', opts = {} },
+    },
+    config = function()
+      vim.diagnostic.config({ -- CHANGED
+        virtual_text = false,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+        callback = function(event)
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client then
+            client.server_capabilities.semanticTokensProvider = nil -- CHANGED
+          end
+
+          local map = function(keys, func, desc)
+            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+        end,
+      })
+    end,
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+  },
+  {
+    "ThePrimeagen/harpoon",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = true,
+  },
+      { -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          -- {
+          --   'rafamadriz/friendly-snippets',
+          --   config = function()
+          --     require('luasnip.loaders.from_vscode').lazy_load()
+          --   end,
+          -- },
+        },
+      },
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds other completion capabilities.
+      --  nvim-cmp does not ship with all sources by default. They are split
+      --  into multiple repos for maintenance purposes.
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+    },
+    config = function()
+      -- See `:help cmp`
+      local cmp = require 'cmp'
+      local luasnip = require 'luasnip'
+      luasnip.config.setup {}
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        completion = { completeopt = 'menu,menuone,noinsert' },
+
+        -- For an understanding of why these mappings were
+        -- chosen, you will need to read `:help ins-completion`
+        --
+        -- No, but seriously. Please read `:help ins-completion`, it is really good!
+        mapping = cmp.mapping.preset.insert {
+          -- Select the [n]ext item
+          -- ['<C-n>'] = cmp.mapping.select_next_item(),
+          -- Select the [p]revious item
+          -- ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+          -- Scroll the documentation window [b]ack / [f]orward
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+          -- Accept ([y]es) the completion.
+          --  This will auto-import if your LSP supports it.
+          --  This will expand snippets if the LSP sent a snippet.
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+          -- If you prefer more traditional completion keymaps,
+          -- you can uncomment the following lines
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+          -- Manually trigger a completion from nvim-cmp.
+          --  Generally you don't need this, because nvim-cmp will display
+          --  completions whenever it has completion options available.
+          ['<C-Space>'] = cmp.mapping.complete {},
+
+          -- Think of <c-l> as moving to the right of your snippet expansion.
+          --  So if you have a snippet that's like:
+          --  function $name($args)
+          --    $body
+          --  end
+          --
+          -- <c-l> will move you to the right of each of the expansion locations.
+          -- <c-h> is similar, except moving you backwards.
+          ['<C-l>'] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { 'i', 's' }),
+          ['<C-h>'] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { 'i', 's' }),
+
+          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'path' },
+				},
+			}
+		end,
+	},
+	{
+		'hrsh7th/nvim-cmp',
+		event = 'InsertEnter',
+		dependencies = {
+			-- {
+			-- 	'L3MON4D3/LuaSnip',
+			-- 	build = (function()
+			-- 		if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+			-- 			return
+			-- 		end
+			-- 		return 'make install_jsregexp'
+			-- 	end)(),
+			-- 	dependencies = {},
+			-- },
+			-- 'saadparwaiz1/cmp_luasnip',
+			-- 'hrsh7th/cmp-nvim-lsp',
+			-- 'hrsh7th/cmp-path',
+		},
+		config = function()
+			local cmp = require 'cmp'
+			local luasnip = require 'luasnip'
+			luasnip.config.setup {}
+
+			cmp.setup {
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				completion = { completeopt = 'menu,menuone,noinsert' },
+				mapping = cmp.mapping.preset.insert {
+					-- Select the [n]ext item
+					-- ['<C-n>'] = cmp.mapping.select_next_item(),
+					-- Select the [p]revious item
+					-- ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+					-- Scroll the documentation window [b]ack / [f]orward
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+					-- Accept ([y]es) the completion.
+					--  This will auto-import if your LSP supports it.
+					--  This will expand snippets if the LSP sent a snippet.
+					['<C-y>'] = cmp.mapping.confirm { select = true },
+
+					-- If you prefer more traditional completion keymaps,
+					-- you can uncomment the following lines
+					['<CR>'] = cmp.mapping.confirm { select = true },
+					['<Tab>'] = cmp.mapping.select_next_item(),
+					['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+					-- Manually trigger a completion from nvim-cmp.
+					--  Generally you don't need this, because nvim-cmp will display
+					--  completions whenever it has completion options available.
+					['<C-Space>'] = cmp.mapping.complete {},
+
+					-- Think of <c-l> as moving to the right of your snippet expansion.
+					--  So if you have a snippet that's like:
+					--  function $name($args)
+					--    $body
+                    --  end
+                    --
+                    -- <c-l> will move you to the right of each of the expansion locations.
+                    -- <c-h> is similar, except moving you backwards.
+                    ['<C-l>'] = cmp.mapping(function()
+                        if luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-h>'] = cmp.mapping(function()
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        end
+                    end, { 'i', 's' }),
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                },
+            }
+        end,
+    },
+    {
+        'nvim-tree/nvim-tree.lua',
+        lazy = false,
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('nvim-tree').setup {
+                view = {
+                    adaptive_size = true,
+                    side = 'left',
+                    preserve_window_proportions = true,
+                },
+                filters = {
+                    dotfiles = false,
+                    git_ignored = false,
+                    custom = { 'node_modules', '.git' },
+                },
+                git = { enable = false },
+                renderer = {
+                    group_empty = true,
+                },
+            }
+            vim.api.nvim_create_autocmd("VimEnter", {
+                callback = function()
+                    -- require("nvim-tree.api").tree.open()
+                    require("nvim-tree")
+                end,
+            })
+            vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<CR>', { desc = 'Toggle nvim-tree' })
+            vim.keymap.set('n', '<leader>r', ':NvimTreeFindFile<CR>', { desc = 'Reveal current file in tree' })
+        end
+    },
+    {
+        'nvimtools/none-ls.nvim',
+        dependencies = { 'nvimtools/none-ls-extras.nvim' },
+        config = function()
+            local null_ls = require('null-ls')
+            local eslint_d      = require('none-ls.diagnostics.eslint_d')
+            local eslint_d_fix  = require('none-ls.code_actions.eslint_d')
+            local prettier_fmt = null_ls.builtins.formatting.prettier.with({
+              extra_args = { "--config", vim.fn.getcwd() .. "/.prettierrc" },
+            })
+
+            null_ls.setup({
+                root_dir = require("null-ls.utils").root_pattern(
+                    ".eslintrc",
+                    ".eslintrc.js",
+                    ".eslintrc.cjs",
+                    ".eslintrc.json",
+                    "package.json",
+                    ".git",
+                    ".prettierrc",
+                    ".prettierrc.js",
+                    ".prettierrc.json"
+                ),
+                sources = {
+                    eslint_d,
+                    eslint_d_fix,
+                    prettier_fmt,
+                },
+                on_attach = function(client, bufnr)
+                    -- no auto-format or lint on save
+                end,
+            })
+        end,
+    },
+    {
+        'lewis6991/gitsigns.nvim',
+        event = 'BufReadPre',
+        config = function()
+            require('gitsigns').setup({
+                signs = {
+                    add          = { text = '+' },
+                    change       = { text = '~' },
+                    delete       = { text = 'x' },
+                    topdelete    = { text = 'p' },
+                    changedelete = { text = 'o' },
+                },
+            })
+        end,
+    }
+})
+
+-- Window navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Harpoon
+vim.keymap.set("n", "<leader>mm", function() require("harpoon.mark").add_file() end)
+vim.keymap.set("n", "<leader>hh", function() require("harpoon.ui").toggle_quick_menu() end)
+vim.keymap.set("n", "<leader>1", function() require("harpoon.ui").nav_file(1) end)
+vim.keymap.set("n", "<leader>2", function() require("harpoon.ui").nav_file(2) end)
+vim.keymap.set("n", "<leader>3", function() require("harpoon.ui").nav_file(3) end)
+vim.keymap.set("n", "<leader>4", function() require("harpoon.ui").nav_file(4) end)
+vim.keymap.set("n", "<leader>5", function() require("harpoon.ui").nav_file(5) end)
+vim.keymap.set("n", "<leader>6", function() require("harpoon.ui").nav_file(6) end)
+vim.keymap.set("n", "<leader>7", function() require("harpoon.ui").nav_file(7) end)
+vim.keymap.set("n", "<leader>8", function() require("harpoon.ui").nav_file(8) end)
+
+-- Clipboard
+vim.keymap.set('v', "<leader>y", "\"*y")
+
+-- Diagnostics
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Other
+vim.keymap.set('n', '<leader>j', '/', { noremap = true })
+vim.cmd([[cnoreabbrev qq wqa]])
+vim.cmd([[cnoreabbrev qqq qa!]])
+
+-- Linting
+vim.keymap.set('n', '<leader>k', function()
+    vim.lsp.buf.code_action({ apply = true })
+end, { desc = 'Apply fix for current issue' })
+
+vim.keymap.set('n', '<leader>K', function()
+    vim.cmd('write')  -- Save the current buffer
+    local file = vim.fn.expand('%')
+    vim.fn.jobstart({ 'eslint_d', '--fix', file }, {
+        stdout_buffered = true,
+        on_stdout = function(_, data)
+            if data then vim.notify(table.concat(data, "\n")) end
+        end,
+        on_exit = function()
+            vim.cmd('edit')  -- Reload buffer after fixing
+        end,
+    })
+end, { desc = 'Full ESLint_d --fix on file' })
